d99fc80e23435f67ec6f85ea191367345261599c
Yog <yog.panjarale@gmail.com>
2021-09-18 23:51:07 +0530

    basic layout

---
+++ frontend/pages/device.tsx
@@ -0,0 +1,69 @@
+import Head from "next/head";
+import { useState } from "react";
+
+function Device() {
+	const [checkBox, setCheckBox] = useState(false);
+	const [engineTemp1, setengineTemp1] = useState(0);
+	const [engineTemp2, setengineTemp2] = useState(0);
+	const [engineTemp3, setengineTemp3] = useState(0);
+	const [vacuumTemp, setVacuumTemp] = useState(0);
+	const [engineRpm, setEngineRpm] = useState(0);
+	const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+		//@ts-ignore
+		const value = e.target?.checked;
+		console.log(value);
+		setCheckBox(value);
+	};
+	return (
+		<>
+			<Head>
+				<title>Remote Fuel Monitoring and Pump Control</title>
+			</Head>
+			<div className="flex justify-center items-start flex-col p-4 pl-6">
+				<h1 className="text-2xl font-bold my-2 text-gray-800">
+					Remote Fuel Monitoring and Pump Control
+				</h1>
+				<hr className="h-2 w-full bg-gray-200 my-2 " />
+				<div>
+					<h2 className="text-xl font-bold text-gray-700 ">
+						Motor Pump Control
+					</h2>
+					<div className="relative inline-block w-10 mr-2 pt-2 align-middle select-none transition duration-200 ease-in ">
+						<input
+							type="checkbox"
+							name="toggle"
+							id="toggle"
+							className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+							onChange={handleChange}
+							checked={checkBox}
+						/>
+						<label
+							htmlFor="toggle"
+							className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+						></label>
+					</div>
+					<label htmlFor="toggle" className="text-xs text-gray-700">
+						Motor On/Off
+					</label>
+					<p className="text-xl text-gray-800 font-bold py-2 rounded">
+						Engine Temperature 1 : {engineTemp1} °C
+					</p>
+					<p className="text-xl text-gray-800 font-bold py-2 rounded">
+						Engine Temperature 2 : {engineTemp2} °C
+					</p>
+					<p className="text-xl text-gray-800 font-bold py-2 rounded">
+						Engine Temperature 3 : {engineTemp3} °C
+					</p>
+					<p className="text-xl text-gray-800 font-bold py-2 rounded">
+						Vacuum Pump Temperature : {vacuumTemp} °C
+					</p>
+					<p className="text-xl text-gray-800 font-bold py-2 rounded">
+						Engine RPM : {engineRpm} RPM
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Device;
