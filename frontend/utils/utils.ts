f7713c28cd5a3f2bfb75503b2efd7e50510441f2
Yog <yog.panjarale@gmail.com>
2021-11-15 22:35:38 +0530

    reset project

--- frontend/utils/utils.ts
+++
@@ -1,3 +0,0 @@
-export function withUrl(input:string){
-    return `${process.env.BACKEND_URL||"http://localhost:3001"}${input}`
-}
--- src/events.ts
+++
@@ -1,5 +0,0 @@
-import EventEmitter from "events";
-
-const emitter = new EventEmitter();
-console.log('events exported')
-export default emitter;
