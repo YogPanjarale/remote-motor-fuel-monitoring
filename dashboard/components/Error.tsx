39e1149f05f9a4a63384282a23f1f0b61088ff1a
Yog <yog.panjarale@gmail.com>
2021-12-23 18:01:56 +0530

    error page

---
+++ dashboard/components/Error.tsx
@@ -0,0 +1,11 @@
+const ErrorPage = ({ statusCode,message }) => {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center text-center">
+                <div className="inline-flex items-center">
+                    <h1 className="text-2xl py-[11px] pr-6 mr-5 inline-block border-gray-400 border-r-[1px]  font-semibold">{statusCode}</h1>
+                    <h2 className="text-sm">{message}</h2>
+                </div>
+        </div>
+    );
+    };
+export default ErrorPage;
---
+++ dashboard/pages/404.tsx
@@ -0,0 +1,6 @@
+import ErrorPage from "../components/Error";
+
+const Page = () => {
+	return <ErrorPage statusCode={404} message="This page could not be found." />;
+};
+export default Page;
