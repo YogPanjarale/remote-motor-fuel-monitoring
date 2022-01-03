4bc7a9359403157d7b501693d0622280894dcd4b
Yog <yog.panjarale@gmail.com>
2022-01-03 20:49:45 +0530

    done lot of stuff

--- device/ArdunioNano/src/main.cpp
+++ device/ArdunioNano/src/main.cpp
@@ -1,39 +1,24 @@
 #include "Arduino.h"
 #include "ArduinoJson.h"
-#include "max6675.h"
-#include "SPI.h"
-const int spi_miso_pin = 12;// SPI master in slave out pin
-const int spi_sck_pin = 13;//SPI serial clock pin
-const byte max6675_num = 4;// the number of max6675s modules connected
-const int max6675_cs_pins[max6675_num] = //max6675 chip select pins 
-{
-  10,
-  9,
-  8,
-  7,
-};
-
-MAX6675 max6675s[max6675_num] = 
-{
-  MAX6675(spi_miso_pin, spi_sck_pin, max6675_cs_pins[0]),
-  MAX6675(spi_miso_pin, spi_sck_pin, max6675_cs_pins[1]),
-  MAX6675(spi_miso_pin, spi_sck_pin, max6675_cs_pins[2]),
-  MAX6675(spi_miso_pin, spi_sck_pin, max6675_cs_pins[3])
-};
-
-const int water_presence_pin = 2;// water presence pin
-const int fuel_level_pin = A0;// fuel level pin
+const byte no_of_temp = 4;          // the number of engine temperature sensors connected
+const int tempSensors[no_of_temp] = {10,9,8,7};// Engine temperature sensor pins
+const int water_presence_pin = 2; // water presence pin
+const int fuel_level_pin = A0;    // fuel level pin
+const int engineRpmPulsePin = 6;  // engine rpm pulse pin
+const int waterFlowPulsePin = 5;  // water flow pulse pin
+const int waterPresencePin = 11; // water presence pulse pin
 
 struct Engine
 {
-  bool water_presence;// water presence
-  int engine_temp1;// engine temperature 1
-  int engine_temp2;// engine temperature 2
-  int engine_temp3;// engine temperature 3
-  int engine_temp4;// engine temperature 4
-  int fuel_sensor;// fuel sensor
+  bool water_presence; // water presence
+  int engine_temp1;    // engine temperature 1
+  int engine_temp2;    // engine temperature 2
+  int engine_temp3;    // engine temperature 3
+  int engine_temp4;    // engine temperature 4
+  int fuel_sensor;     // fuel sensor
   // to doc
-  DynamicJsonDocument toDoc(){
+  DynamicJsonDocument toDoc()
+  {
     DynamicJsonDocument doc = DynamicJsonDocument(1024);
     doc["water_presence"] = water_presence;
     doc["engine_temp1"] = engine_temp1;
@@ -45,11 +30,19 @@ struct Engine
   };
 };
 
+void setTempSensorPinMode()
+{
+  for (int i = 0; i < no_of_temp; i++)
+  {
+    pinMode(tempSensors[i], INPUT);
+  }
+}
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(water_presence_pin,INPUT);
-  pinMode(fuel_level_pin,INPUT);
+  pinMode(water_presence_pin, INPUT);
+  pinMode(fuel_level_pin, INPUT);
   // pinMode(13, OUTPUT);
 }
 void read_water_presence(Engine &engine)
@@ -58,10 +51,10 @@ void read_water_presence(Engine &engine)
 }
 void read_engine_temp(Engine &engine)
 {
-  engine.engine_temp1 = max6675s[0].readCelsius();
-  engine.engine_temp2 = max6675s[1].readCelsius();
-  engine.engine_temp3 = max6675s[2].readCelsius();
-  engine.engine_temp4 = max6675s[3].readCelsius();
+  engine.engine_temp1 = digitalRead(tempSensors[0]);
+  engine.engine_temp2 = digitalRead(tempSensors[1]);
+  engine.engine_temp3 = digitalRead(tempSensors[2]);
+  engine.engine_temp4 = digitalRead(tempSensors[3]);
 }
 void read_fuel_sensor(Engine &engine)
 {
@@ -70,35 +63,12 @@ void read_fuel_sensor(Engine &engine)
   engine.fuel_sensor = voltage;
 }
 
-void loop() {
+void loop()
+{
   Engine engine;
   read_water_presence(engine);
   read_engine_temp(engine);
   read_fuel_sensor(engine);
-  // Serial.println(engine.toDoc().as<char*>());
-  // Serial.println(engine.toDoc().as<String>());
- DynamicJsonDocument inp(1024);
-  if (Serial.available()>0){
-    String input = Serial.readString();
-    deserializeJson(inp, input);
-    if (inp["command"] =="send all"){
-      serializeJson(engine.toDoc(), Serial);
-      digitalWrite(13, HIGH);
-    }
-  }
-    delay(1000);
+  Serial.println(engine.toDoc().as<String>());
+  delay(1000);
 }
-
-
-/*
- DynamicJsonDocument inp(1024);
-  if (Serial.available() > 0) {
-    String input = Serial.readString();
-    deserializeJson(inp, input);
-    Serial.println("You said : " + input);
-    if (inp["command"] == "on") {
-      digitalWrite(13, HIGH);
-    } else if (inp["command"] == "off") {
-      digitalWrite(13, LOW);
-    }
-  }*/
