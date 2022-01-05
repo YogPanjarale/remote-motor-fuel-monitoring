a1ac15120a26a961f4a16f6d1cc6cc4dbce30162
Yog <yog.panjarale@gmail.com>
2022-01-06 01:21:39 +0530

    Made multiple themocouples working

--- device/ArdunioNano/README.md
+++ device/ArdunioNano/README.md
@@ -1,5 +1,12 @@
 # Arduino Nano to take input Sensor data
 
+## Wiring
+
+SCK Serial Clock (SCK) - pin 13
+MISO Master In Slave Out (MISO) - pin 12
+TEMP Sensor 1 CS CHip Select (CS) - pin 10
+TEMP Sensor 2 CS CHip Select (CS) - pin 9
+
 ## Functions
 
 1. Read Temperature from Thermocouples 1,2,3,4
--- device/ArdunioNano/src/main.cpp
+++ device/ArdunioNano/src/main.cpp
@@ -1,27 +1,68 @@
-// Sample Arduino MAX6675 Arduino Sketch
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "SPI.h"
+// #include "millis_delay.h"
+const byte max6675_num = 2;
+const int max6675_cs_pin[max6675_num] = {10,9};
 
-#include "max6675.h"
+SPISettings max6675_spi(1000000, MSBFIRST, SPI_MODE1); //1mhz SPI clock
 
-int ktcSO = 8;
-int ktcCS = 9;
-int ktcCLK = 10;
+void spiSetup(){
 
-MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
-
+  int i;
+  for (i = 0; i < max6675_num; i++) {
+    pinMode(max6675_cs_pin[i], OUTPUT);
+    digitalWrite(max6675_cs_pin[i], HIGH);
+  }
+  SPI.begin();
   
+}
+
 void setup() {
+  spiSetup();
   Serial.begin(9600);
   // give the MAX a little time to settle
   delay(500);
 }
+float max6675Read(int max6675_cs_pin){
+  unsigned int data;
+  SPI.beginTransaction(max6675_spi);
+  digitalWrite(max6675_cs_pin, LOW);
 
-void loop() {
-  // basic readout test
+  data = SPI.transfer16(0);
+
+  digitalWrite(max6675_cs_pin, HIGH);
+  SPI.endTransaction();
+  if (data & 0x00004) {
+    return NAN;
+  }
+  else{
+    return (data >> 3) / 4;
+  }
+}
+void readTemperatures() {
+  const float mov_avg_alpha = 0.1;
+  static float mov_avg[max6675_num] = {-100,-100};
+  float value;
+  int i;
+  delay(220);
+
+  for (i = 0; i < (int)max6675_num; i++)
+  {
+    value = max6675Read(max6675_cs_pin[i]);
+
+    if (mov_avg[i]==-100) mov_avg[i] = value;
+
+    mov_avg[i] = value * mov_avg_alpha +  mov_avg[i] * (1.0 - mov_avg_alpha);
+    Serial.print(round(mov_avg[i]));
+    Serial.print("\t");
+    Serial.println("read");
+
+  }
   
-   Serial.print("Deg C = "); 
-   Serial.print(ktc.readCelsius());
-   Serial.print("t Deg F = ");
-   Serial.println(ktc.readFahrenheit());
- 
+  //TODO: read the temperatures from the MAX
+}
+void loop() {
+  readTemperatures();
    delay(500);
 }
