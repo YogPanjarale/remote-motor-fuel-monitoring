8ddd92bc4e0ebc8b52894fe2f8aafc872bed7234
Yog <yog.panjarale@gmail.com>
2022-01-18 01:21:06 +0530

    esp32 wifi mqtt

--- device/esp32/src/main.cpp
+++ device/esp32/src/main.cpp
@@ -1,9 +1,50 @@
 #include <Arduino.h>
+#include "EspMQTTClient.h"
 
-void setup() {
-  // put your setup code here, to run once:
+EspMQTTClient client(
+  "PANJARALE_HOSPITAL",
+  "12031975",
+  "do1.yogpanjarale.com",
+  "wifi-esp",
+  "Pleaseno",
+  "esp32-wifi-mqtt",
+  (short)1883);
+  
+void setup()
+{
+  Serial.begin(115200);
+
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
+  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// This function is called once everything is connected (Wifi and MQTT)
+// WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
+void onConnectionEstablished()
+{
+  // Subscribe to "mytopic/test" and display received message to Serial
+  client.subscribe("mytopic/test", [](const String & payload) {
+    Serial.println(payload);
+  });
+
+  // Subscribe to "mytopic/wildcardtest/#" and display received message to Serial
+  client.subscribe("mytopic/wildcardtest/#", [](const String & topic, const String & payload) {
+    Serial.println("(From wildcard) topic: " + topic + ", payload: " + payload);
+  });
+
+  // Publish a message to "mytopic/test"
+  client.publish("mytopic/test", "This is a message"); // You can activate the retain flag by setting the third parameter to true
+
+  // Execute delayed instructions
+  client.executeDelayed(5 * 1000, []() {
+    client.publish("mytopic/wildcardtest/test123", "This is a message sent 5 seconds later");
+  });
+}
+
+void loop()
+{
+  client.loop();
+  delay(1000);
 }
