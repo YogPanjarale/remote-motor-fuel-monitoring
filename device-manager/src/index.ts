bc8bd442049412fbea9884af238daf800a600c96
Yog <yog.panjarale@gmail.com>
2022-02-01 22:18:39 +0530

    device manager init

--- device-manager/src/index.ts
+++ device-manager/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Start ig 🤞")
+import * as mqtt from "mqtt"; // import everything inside the mqtt module and give it the namespace "mqtt"
+import { AsyncClient } from "async-mqtt";
+const client = mqtt.connect("mqtt://do1.yogpanjarale.com:1883", {
+	clientId: "my-client-id",
+	username: "dev-man-",
+	password: "Pleaseno",
+});
+const asyncClient = new AsyncClient(client);
+const branch = "0x"
+const devices = ["001"]
+async function main() {
+    await asyncClient.publish("Hello/Wolrd","stuff")
+    asyncClient.subscribe(`devices/${branch}/#`)
+}
+setInterval(() => {
+    devices.forEach(async (device) => {
+        await asyncClient.publish(`devices/${branch}/${device}/update`, "1")
+    })
+}, 5000);
+client.on("message", function (topic, message) {
+	// message is Buffer
+	console.log(`[Incomming message] : ${topic} , ${message.toString()}`);
+	// client.end();
+});
+main();
